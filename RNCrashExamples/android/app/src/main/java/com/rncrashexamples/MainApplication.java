6cc551291f98058b936a822c7d0878f2a508be71
Akhilesh Mourya <amourya@isystango.com>
2018-09-21 19:08:11 +0530

    Removed CodePush key

--- RNCrashExamples/android/app/src/main/java/com/rncrashexamples/MainApplication.java
+++ RNCrashExamples/android/app/src/main/java/com/rncrashexamples/MainApplication.java
@@ -33,7 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new CodePush("RUwxvAUE6hi3Wz4nqNnBzIhNKDbFB11ioIAdX", getApplicationContext(), BuildConfig.DEBUG),
+            new CodePush("null", getApplicationContext(), BuildConfig.DEBUG),
             new RNDeviceInfo(),
               new SQLitePluginPackage()
       );
